a344d53a02e890948bcfae73df7dd6e3a2982205
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-05-03 19:51:54 +0530

    Bhai dekh lo

--- alpha/config.py
+++ alpha/config.py
@@ -1,6 +1,12 @@
 import os
+ENV = bool(os.environ.get("ENV", False))
 
-class Var:
-    API_ID = int(os.environ.get("API_ID", 0))
+if ENV:
+
+    class Config(object):
+
+      API_ID = int(os.environ.get("API_ID", 0))
+
     API_HASH = os.environ.get("API_HASH")
+
     STRING_SESSION = os.environ.get("STRING_SESSIOn")
