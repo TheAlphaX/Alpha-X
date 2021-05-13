955bbec172e5efb46b92861c1e4388e8cbf864e2
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-05-13 10:14:41 +0530

    Add files via upload

---
+++ alpha/__init__.py
@@ -0,0 +1,10 @@
+import os 
+import time
+from .client import alpha
+from alpha.config import Var
+if Var.STRING_SESSION:
+    session_name = str(Var.STRING_SESSION)
+    bot = TelegramClient(StringSession(session_name), Var.APP_ID, Var.API_HASH)
+else:
+    session_name = "startup"
+    bot = TelegramClient(session_name, Var.APP_ID, Var.API_HASH)
