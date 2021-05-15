2b80f329f3d985d3ae3616ae7864ba2275796d3f
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-05-15 20:17:41 +0530

    Update config.py

--- alpha/config.py
+++ alpha/config.py
@@ -1,12 +1,5 @@
 import os
-ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-
-    class Config(object):
-
-      API_ID = int(os.environ.get("API_ID", 0))
-
-    API_HASH = os.environ.get("API_HASH")
-
-    STRING_SESSION = os.environ.get("STRING_SESSIOn")
+API_ID = int(os.environ.get("API_ID", 0))
+API_HASH = os.environ.get("API_HASH")
+STRING_SESSION = os.environ.get("STRING_SESSION")
