9a47ebdb5bb5ffa3958caced548155cfe7d8a1ac
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-05-21 17:37:28 +0530

    Update config.py

--- alpha/config.py
+++ alpha/config.py
@@ -8,4 +8,5 @@ ALIVE_PIC = os.environ.get("ALIVE_PIC")
 CMD_HNDLR = os.environ.get("CMD_HNDLR", r"\.")
 HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
 HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+OWNER_ID = tuple(filter(lambda x: x, map(int, os.environ.get("OWNER_ID", "0").split())))
 
