b07e36ce6b9379e25a14da683519a2b76a944d2a
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-05-21 13:01:27 +0530

    Update client.py

--- alpha/client.py
+++ alpha/client.py
@@ -4,5 +4,6 @@ from pyrogram import Client
 alpha = Client(
     session_name=config.STRING_SESSION,
     api_id=config.API_ID,
-    api_hash=config.API_HASH
+    api_hash=config.API_HASH,
+    plugins={'root': 'plugins'}
 )
