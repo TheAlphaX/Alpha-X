4c00070e17b310f062860d2899289d491fc72aca
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-05-21 16:51:30 +0530

    Update config.py

--- alpha/config.py
+++ alpha/config.py
@@ -3,3 +3,9 @@ import os
 API_ID = int(os.environ.get("API_ID", 0))
 API_HASH = os.environ.get("API_HASH")
 STRING_SESSION = os.environ.get("STRING_SESSION")
+CUSTOM_ALIVE = os.environ.get("CUSTOM_ALIVE")
+ALIVE_PIC = os.environ.get("ALIVE_PIC")
+CMD_HNDLR = os.environ.get("CMD_HNDLR", r"\.")
+HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
+HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
+CU
