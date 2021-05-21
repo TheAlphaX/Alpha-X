a02f5939c2f589e2ad3af8a637ab0e3d0bfe8d86
Krishna-singhal <65902764+Krishna-Singhal@users.noreply.github.com>
2021-05-21 13:02:45 +0530

    Update client.py

--- alpha/client.py
+++ alpha/client.py
@@ -5,5 +5,5 @@ alpha = Client(
     session_name=config.STRING_SESSION,
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    plugins={'root': 'plugins'}
+    plugins={'root': 'alpha.plugins'}
 )
