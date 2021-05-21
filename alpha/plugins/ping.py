30b622df93c2ac0f4ae1802d3b3bc569b550fd4f
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-05-21 18:58:13 +0530

    Update ping.py

--- alpha/plugins/ping.py
+++ alpha/plugins/ping.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message
 @alpha.on_message(filters.command("ping", ".") & filters.me)
 async def ping(_, msg: Message):
 
-    text = "❮**Pong!**❯\n\n\n__ALPHA-X__ツ"
+    text = "❮**Pong!**❯\n\n\n__ALPHA-X__ is **ON!**ツ"
     st = time.time()
 
     await msg.edit(text)
