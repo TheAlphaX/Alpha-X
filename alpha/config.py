11c2c12064f3b3c9cd171c2c9d5457450172fb09
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-05-27 12:02:36 +0530

    Update config.py

--- alpha/config.py
+++ alpha/config.py
@@ -13,6 +13,12 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 SUDO_ENABLED = False
 SUDO_USERS: Set[int] = set()
 DOWN_PATH = os.environ.get("DOWN_PATH")
+U_BRANCH = "main"
+UPSTREAM_REPO = os.environ.get(
+
+        "UPSTREAM_REPO", "https://github.com/TheAlphaX/Alpha-x"
+
+    )
 
 
             
