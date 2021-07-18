98b4210855f1ff3bc8e00bfbe829b39f452609f4
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-18 22:37:28 +0100

    Create constants.py

---
+++ alpha/helpers/constants.py
@@ -0,0 +1,4 @@
+class First:
+    ALIVE = "`Hello! I am alive`"
+    REPO = 'Click <a href="https://github.com/TheAlphaX/Alpha-X">here</a> to open Usebot\'s GitHub page.'
+    CREATOR = 'I was created by my master <a href="https://github.com/TheAlphaX">Alpha</a> 🌹.'
