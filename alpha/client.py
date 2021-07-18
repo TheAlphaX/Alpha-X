d02ba852bd7047160152e353813b6209da47cc6f
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-18 22:24:38 +0100

    Delete client.py

--- alpha/client.py
+++
@@ -1,9 +0,0 @@
-from alpha import config
-from pyrogram import Client
-
-alpha = Client(
-    session_name=config.STRING_SESSION,
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    plugins={'root': 'alpha.plugins'}
-)
