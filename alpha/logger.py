8a75da7cdebe40f6dcd3be384eaa696fb4e8a4b0
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-18 18:51:45 +0100

    Create logger.py

---
+++ alpha/logger.py
@@ -0,0 +1,19 @@
+
+__all__ = ['logging']
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(asctime)s - %(levelname)s] - %(name)s - %(message)s',
+                    datefmt='%d-%b-%y %H:%M:%S',
+                    handlers=[
+                        RotatingFileHandler(
+                            "logs/userge.log", maxBytes=20480, backupCount=10),
+                        logging.StreamHandler()
+                    ])
+
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+logging.getLogger("pyrogram.parser.html").setLevel(logging.ERROR)
+logging.getLogger("pyrogram.session.session").setLevel(logging.ERROR)
+logging.getLogger('googleapiclient.discovery').setLevel(logging.WARNING)
