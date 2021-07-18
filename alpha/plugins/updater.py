6b6b695fc899de3692fb03e7062028726f3f26bd
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-07-18 20:26:56 +0100

    Create updater.py

---
+++ alpha/plugins/updater.py
@@ -0,0 +1,39 @@
+from git import Repo
+from telethon.tl.functions.channels import ExportMessageLinkRequest as GetLink
+
+from . import *
+
+ULTPIC = "resources/logos/core_used.jpg"
+CL = udB.get("INLINE_PIC")
+if CL:
+    ULTPIC = CL
+
+
+@ultroid_cmd(pattern="update$")
+async def _(e):
+    xx = await eor(e, "`Checking for updates...`")
+    m = await updater()
+    branch = (Repo.init()).active_branch
+    if m:
+        x = await asst.send_file(
+            int(udB.get("LOG_CHANNEL")),
+            ULTPIC,
+            caption="• **Update Available** •",
+            force_document=False,
+            buttons=Button.inline("Changelogs", data="changes"),
+        )
+        if not e.client._bot:
+            Link = (await e.client(GetLink(x.chat_id, x.id))).link
+        else:
+            Link = f"https://t.me/{x.chat.id}/{x.id}"
+        await xx.edit(
+            f'<strong><a href="{Link}">[ChangeLogs]</a></strong>',
+            parse_mode="html",
+            link_preview=False,
+        )
+    else:
+        await xx.edit(
+            f'<code>Your BOT is </code><strong>up-to-date</strong><code> with </code><strong><a href="https://github.com/TeamUltroid/Ultroid/tree/{branch}">[{branch}]</a></strong>',
+            parse_mode="html",
+            link_preview=False,
+        )
