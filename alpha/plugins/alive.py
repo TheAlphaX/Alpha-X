5ab29c417ceadaa4e2fa2774c8933896e997657b
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-19 10:06:55 +0530

    Update alive.py

--- alpha/plugins/alive.py
+++ alpha/plugins/alive.py
@@ -13,12 +13,14 @@ ALIVE_PIC = config.ALIVE_PIC if config.ALIVE_PIC else None
 AL_TEXT = config.ALIVE_TEXT if config.ALIVE_TEXT else f"ʜᴇʟʟo {USERNAME} ᴛʜɪs ɪs ᴀʟᴘʜᴀ-x ɪs ɪɴ ʏᴏᴜʀ sᴇʀᴠɪᴄᴇ"
 
 ALIVE=f"""
-❆ ᴀʟᴘʜᴀ ✗ ᴠɪsɪᴏɴ → `0.0.1`
-❆ Pʏᴛʜᴏɴ Vɪsɪᴏɴ → `3.9.5`
-❆ Sᴜᴅᴏ Usᴇr → {config.SUDO_ENABLED}
-❆ Mʏ Mᴀsᴛᴇʀ → @{USERNAME}
+<b>• ALPHA-X</b> vision→ <code> {config.ALPHA} </code>
+<b>• Python</b> Vision→ <code> {config.PY} </code>
+<b>• Sudo User</b>→ {config.SUDO_ENABLED} 
+<b>•❤️My Master</b>❤️→ @{USERNAME} 
+
 ||•|| Security by ALPHA-X ||•||
 """
+
 
 
 @alpha.on_message(filters.command("alive", ".") & filters.me)
