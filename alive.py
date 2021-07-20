e9861909216f11a713230dc6890e4e0a4516e91b
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-20 14:03:41 +0530

    Add files via upload

---
+++ alive.py
@@ -0,0 +1,27 @@
+import re
+import os
+import asyncio
+
+from typing import Tuple, Optional
+from alpha import alpha 
+from alpha import config
+from alpha.plugins import ALPHA, PY
+
+
+USERNAME = config.OWNER_USERNAME
+BOT_PIC = config.BOT_PIC
+
+ALIVE = f"""
+<b>• ALPHA-X</b> vision→ <code> {ALPHA} </code>
+<b>• Python</b> Vision→ <code> {PY} </code>
+<b>• Sudo User</b>→ {config.SUDO_ENABLED} 
+<b>•❤️My Master</b>❤️→ @{USERNAME} 
+||•|| Security by ALPHA-X ||•||
+
+"""
+@alpha.on_message(filters.command("alive", ".") & filters.me)
+async def alive(message: Message):
+      if BOT_PIC:
+          await message.edit(BOT_PIC, "This is Alpha-x", ALIVE)
+      else:
+          await message.edit("This is ALPHA-X", ALIVE)
