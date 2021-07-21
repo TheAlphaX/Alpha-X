81a9fff8499d539524262b1d8057fae5071ba3c1
loverboyXD <79841865+loverboyXD@users.noreply.github.com>
2021-07-21 17:54:01 +0530

    Update client.py

--- alpha/client.py
+++ alpha/client.py
@@ -1,8 +1,8 @@
 from alpha import config
 from pyrogram import Client
 
-alpha = Client(
-    "Simple-Pyrogram-Bot",
+pm = Client(
+    "PM-BoT",
     bot_token=config.TOKEN,
     api_id=config.API_ID,
     api_hash=config.API_HASH,
